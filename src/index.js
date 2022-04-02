bf53382b1254437c7dc51d6d8bca97ae0efb298a
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2022-04-02 12:38:41 +0530

    registration context provider added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthContextProvider } from "./context/authContext/AuthContext";
 import { MovieContextProvider } from "./context/movieContext/MovieContext";
-import { ListContextProvider } from "./context/listContext/ListContext"
+import { ListContextProvider } from "./context/listContext/ListContext";
+import { RegisterContextProvider } from "./context/registerContext/RegisterContext";
 
 ReactDOM.render(
   <AuthContextProvider>
-    <MovieContextProvider>
-      <ListContextProvider>
-        <App />
-      </ListContextProvider>
-    </MovieContextProvider>
+    <RegisterContextProvider>
+      <MovieContextProvider>
+        <ListContextProvider>
+          <App />
+        </ListContextProvider>
+      </MovieContextProvider>
+    </RegisterContextProvider>
   </AuthContextProvider>,
   document.getElementById('root')
 );
